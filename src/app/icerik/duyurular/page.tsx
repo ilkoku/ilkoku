acb7240639906088fe88e91db3458ec045d84bfd
ilkoku <ilkoku@ilkoku.com>
2026-08-14 00:34:07 +0300

    Add announcements page

---
+++ src/app/icerik/duyurular/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <section className="content-editor-page"><div className="content-page-heading"><div><span>İçerik</span><h1>Duyurular</h1><p>Platform ve bakım duyurularını yönetin.</p></div></div><div className="content-panel"><p>Duyuru yönetimi</p></div></section>;
+}

131301641e2431dc3ccd658e63ab2933e54ebc51
ilkoku <ilkoku@ilkoku.com>
2026-08-14 01:00:10 +0300

    Add CMS content settings defaults

---
+++ src/lib/cms-settings.ts
@@ -0,0 +1,10 @@
+export const cmsSettings = {
+  defaultStatus: "draft",
+  requireExplicitPublish: true,
+  keepRevisions: true,
+  maxRevisionHistory: 100,
+  defaultIndexing: true,
+  requireSeoTitle: false,
+  requireSeoDescription: false,
+  allowContentManagerPublish: false,
+} as const;

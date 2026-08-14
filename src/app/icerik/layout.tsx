dddf124477279bd431432a71cea0827153e3dc54
ilkoku <ilkoku@ilkoku.com>
2026-08-14 11:31:19 +0300

    Pass CMS admin scope to shell

--- src/app/icerik/layout.tsx
+++ src/app/icerik/layout.tsx
@@ -18,6 +18,7 @@ export default async function ContentLayout({
 
   return (
     <ContentShell
+      isAdmin={access.isAdmin}
       user={{
         email: user.email,
         fullName: user.displayName || user.fullName,

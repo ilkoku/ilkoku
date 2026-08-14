d604f4d2973b93831c454c61c2c4830c840fef01
ilkoku <ilkoku@ilkoku.com>
2026-08-14 18:04:22 +0300

    Make legal CMS index locale-aware

--- src/app/icerik/yasal/page.tsx
+++ src/app/icerik/yasal/page.tsx
@@ -1,5 +1,10 @@
 import { CmsDocumentIndex } from "@/components/content/CmsDocumentIndex";
+import { isCmsLocaleEnabled } from "@/lib/cms-locale-state";
+import { normalizeCmsLocale } from "@/lib/cms-locales";
 
-export default function Page() {
-  return <CmsDocumentIndex />;
+export default async function Page({ searchParams }: { searchParams: Promise<{ dil?: string }> }) {
+  const params = await searchParams;
+  const locale = normalizeCmsLocale(params.dil);
+  const localeEnabled = await isCmsLocaleEnabled(locale);
+  return <CmsDocumentIndex locale={locale} localeEnabled={localeEnabled} />;
 }

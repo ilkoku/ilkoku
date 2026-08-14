949529ca25a1ceb35701455530964dd5c7572974
ilkoku <ilkoku@ilkoku.com>
2026-08-14 18:06:24 +0300

    Pass locale into guide editor

--- src/app/icerik/rehber/[id]/page.tsx
+++ src/app/icerik/rehber/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { CmsGuideEditor } from "@/components/content/CmsGuideEditor";
+import { normalizeCmsLocale } from "@/lib/cms-locales";
 
-type PageProps = { params: Promise<{ id: string }> };
+type PageProps = {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{ dil?: string }>;
+};
 
 export const dynamic = "force-dynamic";
 
-export default async function EditGuidePage({ params }: PageProps) {
+export default async function EditGuidePage({ params, searchParams }: PageProps) {
   const { id } = await params;
-  return <CmsGuideEditor id={id} />;
+  const query = await searchParams;
+  const locale = normalizeCmsLocale(query.dil);
+  return <CmsGuideEditor id={id} locale={locale} />;
 }

75c4e4b06b83926a8760eba209f9170765eb99cc
ilkoku <ilkoku@ilkoku.com>
2026-08-17 10:22:29 +0300

    Load writer paper experience on works workspace

---
+++ src/app/eserlerim/layout.tsx
@@ -0,0 +1,9 @@
+import "@/features/writer/writer-paper-experience.css";
+
+export default function WriterWorksLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return children;
+}

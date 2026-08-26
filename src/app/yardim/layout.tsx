8ec94b3a0fbe8d4ad3b63fc1ae7e75a9407ccfc4
ilkoku <ilkoku@ilkoku.com>
2026-08-26 22:12:19 +0300

    Connect help center to shared public shell

--- src/app/yardim/layout.tsx
+++ src/app/yardim/layout.tsx
@@ -1,7 +1,15 @@
 import type { ReactNode } from "react";
 
+import { PublicTrustFooter } from "@/components/content/PublicTrustFooter";
 import { PublicSiteFrame } from "@/components/layout/PublicSiteFrame";
+import "../nasil-calisir/public-trust-footer.css";
+import "./help.css";
 
 export default function PublicHelpLayout({ children }: { children: ReactNode }) {
-  return <PublicSiteFrame>{children}</PublicSiteFrame>;
+  return (
+    <PublicSiteFrame>
+      {children}
+      <PublicTrustFooter />
+    </PublicSiteFrame>
+  );
 }

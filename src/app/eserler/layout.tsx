badb1e1e21e1907aa76b377d576fe31767dcca4a
ilkoku <ilkoku@ilkoku.com>
2026-08-26 17:19:56 +0300

    Show example works on public library

--- src/app/eserler/layout.tsx
+++ src/app/eserler/layout.tsx
@@ -3,14 +3,18 @@ import type { ReactNode } from "react";
 import { PublicTrustFooter } from "@/components/content/PublicTrustFooter";
 import { PublicSiteFrame } from "@/components/layout/PublicSiteFrame";
 
+import { PublicWorksExampleShowcase } from "./PublicWorksExampleShowcase";
+
 import "../nasil-calisir/public-trust-footer.css";
 import "./reference-hero.css";
 import "./reference-content.css";
+import "./public-works-example-showcase.css";
 
 export default function PublicWorksLayout({ children }: { children: ReactNode }) {
   return (
     <PublicSiteFrame>
       {children}
+      <PublicWorksExampleShowcase />
       <PublicTrustFooter />
     </PublicSiteFrame>
   );

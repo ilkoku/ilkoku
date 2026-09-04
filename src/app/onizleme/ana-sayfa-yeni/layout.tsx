d051666d2ffc9d924ce4c841c8d23f32d0713b6c
ilkoku <ilkoku@ilkoku.com>
2026-09-04 11:37:01 +0300

    Load mini terminal login treatment after header styles

--- src/app/onizleme/ana-sayfa-yeni/layout.tsx
+++ src/app/onizleme/ana-sayfa-yeni/layout.tsx
@@ -8,6 +8,7 @@ import "./passport-dark-priority.css";
 import "./why-uniform.css";
 import "./header-encyclopedia.css";
 import "./header-terminal-spine.css";
+import "./header-login-terminal.css";
 
 export default function HomepagePreviewLayout({ children }: { children: ReactNode }) {
   return children;

bb9bac4e97d19782351b13fdeaf87bff87f11306
ilkoku <ilkoku@ilkoku.com>
2026-09-11 16:50:13 +0300

    Use client-safe static media types

--- src/components/content/SiteStaticMediaInventory.tsx
+++ src/components/content/SiteStaticMediaInventory.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import type { CmsStaticMediaSection } from "@/lib/cms-static-media";
+import type { CmsStaticMediaSection } from "@/lib/cms-static-media-types";
 import styles from "./SiteStaticMediaInventory.module.css";
 
 type Props = {

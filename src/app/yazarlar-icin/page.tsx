455f5c845947019f9d2a562ab77026a242db720c
ilkoku <ilkoku@ilkoku.com>
2026-09-13 14:42:53 +0300

    Load education hub card styling

--- src/app/yazarlar-icin/page.tsx
+++ src/app/yazarlar-icin/page.tsx
@@ -9,6 +9,7 @@ import { getPublishedCmsPublicPageState } from "@/lib/cms-public-page-store";
 
 import "@/app/nasil-calisir/how-it-works.css";
 import "@/app/nasil-calisir/public-trust-footer.css";
+import "./education-hub.css";
 import "./for-writers.css";
 import "./role-illustrations.css";
 

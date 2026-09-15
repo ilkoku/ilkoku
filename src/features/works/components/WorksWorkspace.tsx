69732615b91c7245ba2eedb32008744ded3fa515
ilkoku <ilkoku@ilkoku.com>
2026-09-15 22:57:29 +0300

    Show independent work access state in workspace

--- src/features/works/components/WorksWorkspace.tsx
+++ src/features/works/components/WorksWorkspace.tsx
@@ -412,6 +412,11 @@ export function WorksWorkspace({
 
                     <dl>
                       <div>
+                        <dt>Erişim</dt>
+                        <dd>{work.isActive ? "Aktif" : "Pasif"}</dd>
+                      </div>
+
+                      <div>
                         <dt>Bölüm</dt>
                         <dd>
                           {

674d02519873d6e8389bf74de9261094062d2553
ilkoku <ilkoku@ilkoku.com>
2026-09-16 23:06:23 +0300

    Mount live formatting layer in works route

--- src/app/eserlerim/layout.tsx
+++ src/app/eserlerim/layout.tsx
@@ -4,6 +4,7 @@ import { WriterClassificationStatsEnhancer } from "@/features/writer/components/
 import { WriterEditingTools } from "@/features/writer/components/WriterEditingTools";
 import { WriterFullBookPublicationEnhancer } from "@/features/writer/components/WriterFullBookPublicationEnhancer";
 import { WriterFullBookPublicationSubmitBridge } from "@/features/writer/components/WriterFullBookPublicationSubmitBridge";
+import { WriterLiveFormattingLayer } from "@/features/writer/components/WriterLiveFormattingLayer";
 import { WriterPagedManuscriptEnhancer } from "@/features/writer/components/WriterPagedManuscriptEnhancer";
 import { WriterPublishExperienceEnhancer } from "@/features/writer/components/WriterPublishExperienceEnhancer";
 import { WriterRichTextFormattingTools } from "@/features/writer/components/WriterRichTextFormattingTools";
@@ -15,6 +16,7 @@ import "@/features/writer/writer-editor-density.css";
 import "@/features/writer/writer-editing-tools.css";
 import "@/features/writer/writer-text-editing-commands.css";
 import "@/features/writer/writer-rich-text-formatting.css";
+import "@/features/writer/writer-live-formatting-layer.css";
 import "@/features/writer/writer-toolbar-wrap.css";
 import "@/features/writer/writer-paged-manuscript.css";
 import "@/features/writer/writer-publish-experience.css";
@@ -34,6 +36,7 @@ export default function WriterWorksLayout({
       {children}
       <WriterEditingTools />
       <WriterRichTextFormattingTools />
+      <WriterLiveFormattingLayer />
       <WriterTextEditingCommands />
       <WriterPagedManuscriptEnhancer />
       <WriterPublishExperienceEnhancer />

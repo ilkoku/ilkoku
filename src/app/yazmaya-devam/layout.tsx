69c13f5597377e3d97a772dac70832a091491fb9
ilkoku <ilkoku@ilkoku.com>
2026-09-16 22:15:52 +0300

    Mount rich Writer tools on continue route

--- src/app/yazmaya-devam/layout.tsx
+++ src/app/yazmaya-devam/layout.tsx
@@ -1,4 +1,14 @@
-import { WriterRouteEnhancers } from "@/features/writer/components/WriterRouteEnhancers";
+import { WriterBookStructureRefreshBoundary } from "@/features/writer/components/WriterBookStructureRefreshBoundary";
+import { WriterBookTrashEnhancer } from "@/features/writer/components/WriterBookTrashEnhancer";
+import { WriterClassificationStatsEnhancer } from "@/features/writer/components/WriterClassificationStatsEnhancer";
+import { WriterEditingTools } from "@/features/writer/components/WriterEditingTools";
+import { WriterFullBookPublicationEnhancer } from "@/features/writer/components/WriterFullBookPublicationEnhancer";
+import { WriterFullBookPublicationSubmitBridge } from "@/features/writer/components/WriterFullBookPublicationSubmitBridge";
+import { WriterPagedManuscriptEnhancer } from "@/features/writer/components/WriterPagedManuscriptEnhancer";
+import { WriterPublishExperienceEnhancer } from "@/features/writer/components/WriterPublishExperienceEnhancer";
+import { WriterPublishFeedbackBridge } from "@/features/writer/components/WriterPublishFeedbackBridge";
+import { WriterRichTextFormattingTools } from "@/features/writer/components/WriterRichTextFormattingTools";
+import { WriterTextEditingCommands } from "@/features/writer/components/WriterTextEditingCommands";
 import "@/features/writer/writer-paper-experience.css";
 import "@/features/writer/writer-brand-purple.css";
 import "@/styles/light-purple-route-fallback.css";
@@ -23,7 +33,17 @@ export default function ContinueWritingLayout({
   return (
     <>
       {children}
-      <WriterRouteEnhancers includePublishFeedback />
+      <WriterEditingTools />
+      <WriterRichTextFormattingTools />
+      <WriterTextEditingCommands />
+      <WriterPagedManuscriptEnhancer />
+      <WriterPublishExperienceEnhancer />
+      <WriterPublishFeedbackBridge />
+      <WriterFullBookPublicationSubmitBridge />
+      <WriterFullBookPublicationEnhancer />
+      <WriterBookStructureRefreshBoundary />
+      <WriterBookTrashEnhancer />
+      <WriterClassificationStatsEnhancer />
     </>
   );
 }

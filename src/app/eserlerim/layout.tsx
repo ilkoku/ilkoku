3713c4aedc3dafa11a034b09357336baf495b433
ilkoku <ilkoku@ilkoku.com>
2026-09-16 22:08:23 +0300

    Mount shared Writer tools on works route

--- src/app/eserlerim/layout.tsx
+++ src/app/eserlerim/layout.tsx
@@ -1,18 +1,12 @@
-import { WriterBookStructureRefreshBoundary } from "@/features/writer/components/WriterBookStructureRefreshBoundary";
-import { WriterBookTrashEnhancer } from "@/features/writer/components/WriterBookTrashEnhancer";
-import { WriterClassificationStatsEnhancer } from "@/features/writer/components/WriterClassificationStatsEnhancer";
-import { WriterEditingTools } from "@/features/writer/components/WriterEditingTools";
-import { WriterFullBookPublicationEnhancer } from "@/features/writer/components/WriterFullBookPublicationEnhancer";
-import { WriterFullBookPublicationSubmitBridge } from "@/features/writer/components/WriterFullBookPublicationSubmitBridge";
-import { WriterPagedManuscriptEnhancer } from "@/features/writer/components/WriterPagedManuscriptEnhancer";
-import { WriterPublishExperienceEnhancer } from "@/features/writer/components/WriterPublishExperienceEnhancer";
-import { WriterTextEditingCommands } from "@/features/writer/components/WriterTextEditingCommands";
+import { WriterRouteEnhancers } from "@/features/writer/components/WriterRouteEnhancers";
 import "@/features/writer/writer-paper-experience.css";
 import "@/features/writer/writer-brand-purple.css";
 import "@/styles/light-purple-route-fallback.css";
 import "@/features/writer/writer-editor-density.css";
 import "@/features/writer/writer-editing-tools.css";
 import "@/features/writer/writer-text-editing-commands.css";
+import "@/features/writer/writer-rich-text-formatting.css";
+import "@/features/writer/writer-toolbar-wrap.css";
 import "@/features/writer/writer-paged-manuscript.css";
 import "@/features/writer/writer-publish-experience.css";
 import "@/features/writer/writer-publish-review-contrast.css";
@@ -29,15 +23,7 @@ export default function WriterWorksLayout({
   return (
     <>
       {children}
-      <WriterEditingTools />
-      <WriterTextEditingCommands />
-      <WriterPagedManuscriptEnhancer />
-      <WriterPublishExperienceEnhancer />
-      <WriterFullBookPublicationSubmitBridge />
-      <WriterFullBookPublicationEnhancer />
-      <WriterBookStructureRefreshBoundary />
-      <WriterBookTrashEnhancer />
-      <WriterClassificationStatsEnhancer />
+      <WriterRouteEnhancers />
     </>
   );
 }

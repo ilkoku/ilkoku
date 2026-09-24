6b1ec5fd5e99244f3226ca0bb7efd88c07b59d06
ilkoku <ilkoku@ilkoku.com>
2026-09-24 13:31:23 +0300

    Use canonical Next navigation after publishing

--- src/features/writer/components/WriterPublishExperienceEnhancer.tsx
+++ src/features/writer/components/WriterPublishExperienceEnhancer.tsx
@@ -359,10 +359,6 @@ export function WriterPublishExperienceEnhancer() {
       <Link
         className="button button--primary writer-post-publish-action"
         href="/eserlerim"
-        onClick={(event) => {
-          event.preventDefault();
-          window.location.assign("/eserlerim");
-        }}
       >
         Eserlerime Dön
       </Link>,

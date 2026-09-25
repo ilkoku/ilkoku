f0516b601ed87f3f87a9e831ae52b794cb3c90d3
ilkoku <ilkoku@ilkoku.com>
2026-09-25 20:06:02 +0300

    Track Book Index traffic in shared public layout

--- src/app/en-cok-satanlar/layout.tsx
+++ src/app/en-cok-satanlar/layout.tsx
@@ -1,12 +1,14 @@
 import type { ReactNode } from "react";
 
 import { PublicTrustFooter } from "@/components/content/PublicTrustFooter";
+import { BookIndexAnalytics } from "@/features/book-index/public/BookIndexAnalytics";
 import { PublicSiteFrame } from "@/components/layout/PublicSiteFrame";
 import "../nasil-calisir/public-trust-footer.css";
 
 export default function BookIndexLayout({ children }: { children: ReactNode }) {
   return (
     <PublicSiteFrame>
+      <BookIndexAnalytics />
       {children}
       <PublicTrustFooter />
     </PublicSiteFrame>

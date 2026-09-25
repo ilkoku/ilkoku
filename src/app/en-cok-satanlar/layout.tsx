e102b45268bceffe83883660d87a5aeb67ff580b
ilkoku <ilkoku@ilkoku.com>
2026-09-25 19:33:19 +0300

    Mount Book Index in shared public site frame

---
+++ src/app/en-cok-satanlar/layout.tsx
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react";
+
+import { PublicTrustFooter } from "@/components/content/PublicTrustFooter";
+import { PublicSiteFrame } from "@/components/layout/PublicSiteFrame";
+import "../nasil-calisir/public-trust-footer.css";
+
+export default function BookIndexLayout({ children }: { children: ReactNode }) {
+  return (
+    <PublicSiteFrame>
+      {children}
+      <PublicTrustFooter />
+    </PublicSiteFrame>
+  );
+}
